19d8a7be580bbb87b0dbab48abe2fbe9cc9c46b9
Falamansari <130029012+Falamansari@users.noreply.github.com>
2025-06-17 02:24:34 +0000

    conversation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+//let score = "33abc"
+let score = null
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let isloggedIn = 1
+let booleanIsloggedIn =boolean(isloggedIn)
+console.log(booleanIsloggedIn);
+
+
+// 1 => true ; 0 => false
+
+let someNumber = String(someNumber)
+console.log(someNumber);
