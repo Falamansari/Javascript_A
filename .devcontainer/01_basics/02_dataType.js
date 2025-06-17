d988c1bb2ff325ba4e46c41bb02018dea3bcb5ae
Falamansari <130029012+Falamansari@users.noreply.github.com>
2025-06-17 01:58:03 +0000

    Datatype

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as newer version.
+alert ("hello")
+// alert(4+4) we are using nodejs, not browser.
+
+console.log(3+3); console.log("firoz") //bad writing of code
+
+
+let name = "feros"
+let age = 30
+let isloggedIn  = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+// string ""
+
+//boolean => true/false
+//null = standalone value 
+//undefined =>
+//symbol
+//unique
+
+
+//Object
+console.log(typeof age); // undefined
+console.log(typeof null); //object 
+
